9c305539074be267c77233a3bffb56806376bd18
Sergey Zhiltsov <zhiltsov.s.i@gmail.com>
2018-08-21 00:59:08 +0300

    Print any str number

--- src/main.ino
+++ src/main.ino
@@ -7,7 +7,7 @@ int counter = 0;
 int len = 2;
 
 void setup() {
-    DDRB = B111111;
+    DDRB = B1111;
     PORTB = B0000;
 
     pinMode(dsPin, OUTPUT);
@@ -20,20 +20,48 @@ void setup() {
         digitalWrite(stcpPin, HIGH);
         digitalWrite(stcpPin, LOW);
     }
+    Serial.begin(9600);
+}
+
+int currentSymbol = 4;
+String value = " 777";
+int binV[] = {B11111100, B01100000, B11011010, B11110010, B01100110, B10110110, B10111110, B11100000, B11111110, B11110110, B11101110, B00111110, B10011100};
+char strV[] = "0123456789";
+unsigned long lastRun = 0;
+unsigned long lastRun2 = 0;
+
+int getMap(int v) {
+    for (int i = 0; i < sizeof(strV); ++i) {
+        if (strV[i] == v) {
+            return binV[i];
+        }
+    }
+    return B00000001;
+}
+
+void tickDisplayRender() {
+    currentSymbol = (currentSymbol + 1) % 4;
+    char symbol = value[currentSymbol];
+    int binValue = getMap(symbol);
+    for (int i = 0; i < 8; ++i) {
+        if (binValue % 2 == 1) {
+            digitalWrite(dsPin, HIGH);
+        } else {
+            digitalWrite(dsPin, LOW);
+        }
+        digitalWrite(shcpPin, HIGH);
+        digitalWrite(shcpPin, LOW);
+
+
+        binValue = binValue / 2;
+    }
+    PORTB = B1111;
+    digitalWrite(stcpPin, HIGH);
+    digitalWrite(stcpPin, LOW);
+    PORTB = (~(1 << currentSymbol));
 }
 
 void loop() {
-    if (counter == 0) {
-        digitalWrite(dsPin, HIGH);
-    } else {
-        digitalWrite(dsPin, LOW);
-    }
-    digitalWrite(shcpPin, HIGH);
-    digitalWrite(shcpPin, LOW);
-
-    digitalWrite(stcpPin, HIGH);
-    digitalWrite(stcpPin, LOW);
-
-    counter = (counter + 1) % len;
-    delay(200);
+    tickDisplayRender();
+    delay(5);
 }
