436ff4515c73c8c9476e5af5630cc61e00fea792
Sergey Zhiltsov <zhiltsov.s.i@gmail.com>
2018-09-24 23:08:42 +0300

    Add second shift register

--- src/Display.cpp
+++ src/Display.cpp
@@ -9,9 +9,6 @@ Display::Display(int dsPin, int shcpPin, int stcpPin) {
 }
 
 void Display::init() {
-    DDRB = B1111;
-    PORTB = B0000;
-
     pinMode(_dsPin, OUTPUT);
     pinMode(_stcpPin, OUTPUT);
     pinMode(_shcpPin, OUTPUT);
@@ -23,14 +20,14 @@ void Display::init() {
     digitalWrite(_stcpPin, HIGH);
     digitalWrite(_stcpPin, LOW);
     setValue("0000");
+    _currentIndex = 3;
 }
 
-int binV[] = {B11111100, B01100000, B11011010, B11110010, B01100110, B10110110, B10111110, B11100000, B11111110,
-              B11110110, B00000000};
+uint8_t binV[] = {B11111100, B01100000, B11011010, B11110010, B01100110, B10110110, B10111110, B11100000, B11111110,
+                  B11110110, B00000000};
 String strV = "0123456789 ";
 
-
-int getMap(char v) {
+uint8_t getDigitMap(char v) {
     int i = strV.indexOf(v);
     if (i == -1) {
         return B00000001;
@@ -39,11 +36,22 @@ int getMap(char v) {
     }
 }
 
+uint8_t getSectionMap(int index) {
+    return ~(1 << index) & B01111111;
+}
+
+uint16_t getMap(char v, int index) {
+    uint8_t digit = getDigitMap(v);
+    uint8_t section = getSectionMap(index);
+    uint16_t result = (((uint16_t) section) << 8) | digit;
+    return result;
+}
+
 void Display::tick() {
     _currentIndex = (_currentIndex + 1) % 4;
-    char symbol = _value[_currentIndex];
-    int binValue = getMap(symbol);
-    for (int i = 0; i < 8; ++i) {
+    char symbol = _value[3 - _currentIndex];
+    int binValue = getMap(symbol, _currentIndex);
+    for (int i = 0; i < 16; ++i) {
         if (binValue % 2 == 1) {
             digitalWrite(_dsPin, HIGH);
         } else {
@@ -55,10 +63,8 @@ void Display::tick() {
 
         binValue = binValue / 2;
     }
-    PORTB = B1111;
     digitalWrite(_stcpPin, HIGH);
     digitalWrite(_stcpPin, LOW);
-    PORTB = (~(1 << _currentIndex));
 }
 
 void Display::setValue(String value) {
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ int stcpPin = 3; // sync internal -> external
 Display display(dsPin, shcpPin, stcpPin);
 
 void setup() {
+    Serial.begin(9600);
     display.init();
 }
 
